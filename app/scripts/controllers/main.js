8643b1d064daee1524153030209d229cc108070d
yosuke-furukawa <yosuke.furukawa@gmail.com>
2014-01-15 21:27:34 +0900

    fix scrolling request

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,8 +3,9 @@
 var app = angular.module('angularKoaApp');
 app.controller('MainCtrl',[ '$scope', 'Tweets', function ($scope, Tweets) {
   $scope.tweet = {};
-  $scope.lastTweet = {};
+  $scope.lastTweet = { _id : Infinity};
   $scope.tweets = Tweets.query();
+  $scope.onRequest = false;
   $scope.update = function() {
     Tweets.save($scope.tweet, function() {
       $scope.tweets = Tweets.query();
@@ -14,10 +15,14 @@ app.controller('MainCtrl',[ '$scope', 'Tweets', function ($scope, Tweets) {
   $scope.loadMore = function() {
     if ($scope.tweets.length < 9) return;
     var lastTweet = $scope.tweets[$scope.tweets.length - 1];
-    if (lastTweet._id !== $scope.lastTweet._id) {
+    console.log('last tweet', lastTweet._id);
+    console.log('scope last tweet', $scope.lastTweet._id);
+    if (!$scope.onRequest && lastTweet._id < $scope.lastTweet._id) {
+      $scope.onRequest = true;
       var tweets = Tweets.query({lastId : lastTweet._id}, function(){
         $scope.tweets = $scope.tweets.concat(tweets);
         $scope.lastTweet = lastTweet;
+        $scope.onRequest = false;
       });
     }
   };
